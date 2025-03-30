7d8b89ce9b4088ddea974d795d46e783deb62a29
Frode Randers <frode.randers@gmail.com>
2025-03-30 21:28:19 +0200

    Trunkering av HTML-text

--- mining/classify.py
+++ mining/classify.py
@@ -15,14 +15,22 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 file_path = "../sfbreader/data/SFB-flat.json"
 model = SentenceTransformer("KBLab/sentence-bert-swedish-cased")
 
+def truncate_lines(text: str, max_len: int = 40, max_lines: int = None) -> str:
+    lines = text.split("\n")
+    if max_lines:
+        lines = lines[:max_lines]
+    truncated = [line[:max_len] + ("…" if len(line) > max_len else "") for line in lines]
+    return "<br>".join(truncated)
 
 def interactive_plot_clusters(embeddings_2d, labels, texts, extra_info=None, filename=None):
+    texts_html = [truncate_lines(t, max_len=80, max_lines=10) for t in texts]
+
     # Prepare DataFrame
     df = pd.DataFrame({
         "x": embeddings_2d[:, 0],
         "y": embeddings_2d[:, 1],
         "cluster": labels,
-        "text": texts
+        "text": texts_html
     })
 
     # Add extra columns if provided
@@ -71,8 +79,8 @@ def plot_clusters(embeddings_2d, labels, title="UMAP + HDBSCAN Clusters", filena
     if filename:
         plt.savefig(filename, dpi=300)
         print(f"Lagrat till: {filename}")
-
-    plt.show()
+    else:
+        plt.show()
 
 def main():
     texts = []
@@ -190,7 +198,8 @@ def main():
 
     extra_info = {
         "kapitel": [r["kapitel"] for r in data],
-        "paragraf": [r["paragraf"] for r in data]
+        "paragraf": [r["paragraf"] for r in data],
+        "stycke": [r["stycke"] for r in data]
         #"rubrik": [r.get("paragraf_rubrik", "") for r in data]
     }
 
